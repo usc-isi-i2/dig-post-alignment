b347a1955af8a5b2e06194550af05d2ca9cb59c1
Amandeep Singh <amandeep.s.saggu@gmail.com>
2015-09-29 16:17:21 -0700

    output path based on classname

--- python/preclustering-spark.py
+++ python/preclustering-spark.py
@@ -8,7 +8,6 @@ from partitionht import PartitionHt
 
 if __name__ == "__main__":
 
-
     AdultService = "AdultService"
     PhoneNumber = "PhoneNumber"
     Offer = "Offer"
@@ -35,17 +34,17 @@ if __name__ == "__main__":
     input_rdd = fileUtil.load_json_file(inputFilename, inputFileFormat, c_options)
     partition = PartitionHt()
 
-    as_rdd = input_rdd.mapValues(lambda x: partition.filter_docs(AdultService,x)).filter(lambda  x: x[1] is not None)
-    pn_rdd = input_rdd.mapValues(lambda x: partition.filter_docs(PhoneNumber,x)).filter(lambda x: x[1] is not None)
-    o_rdd =  input_rdd.mapValues(lambda x: partition.filter_docs(Offer,x)).filter(lambda x: x[1] is not None)
-    po_rdd = input_rdd.mapValues(lambda x: partition.filter_docs(PersonOrOrganization,x)).filter(lambda x: x[1] is not None)
-    wp_rdd = input_rdd.mapValues(lambda x: partition.filter_docs(WebPage,x)).filter(lambda x: x[1] is not None)
-    ea_rdd = input_rdd.mapValues(lambda x: partition.filter_docs(EmailAddress,x)).filter(lambda x: x[1] is not None)
+    as_rdd = input_rdd.mapValues(lambda x: partition.filter_docs(AdultService, x)).filter(lambda x: x[1] is not None)
+    pn_rdd = input_rdd.mapValues(lambda x: partition.filter_docs(PhoneNumber, x)).filter(lambda x: x[1] is not None)
+    o_rdd =  input_rdd.mapValues(lambda x: partition.filter_docs(Offer, x)).filter(lambda x: x[1] is not None)
+    po_rdd = input_rdd.mapValues(lambda x: partition.filter_docs(PersonOrOrganization, x)).filter(lambda x: x[1] is not None)
+    wp_rdd = input_rdd.mapValues(lambda x: partition.filter_docs(WebPage, x)).filter(lambda x: x[1] is not None)
+    ea_rdd = input_rdd.mapValues(lambda x: partition.filter_docs(EmailAddress, x)).filter(lambda x: x[1] is not None)
 
-    fileUtil.save_text_file(as_rdd, outputPath + "/adultservice", outputFileFormat, c_options)
-    fileUtil.save_text_file(pn_rdd, outputPath + "/phonenumber", outputFileFormat, c_options)
-    fileUtil.save_text_file(o_rdd, outputPath + "/offer", outputFileFormat, c_options)
-    fileUtil.save_text_file(po_rdd, outputPath + "/personororganization", outputFileFormat, c_options)
-    fileUtil.save_text_file(wp_rdd, outputPath + "/webpage", outputFileFormat, c_options)
-    fileUtil.save_text_file(ea_rdd, outputPath + "/emailaddress", outputFileFormat, c_options)
+    fileUtil.save_text_file(as_rdd, outputPath + "/"+ AdultService, outputFileFormat, c_options)
+    fileUtil.save_text_file(pn_rdd, outputPath + "/" + PhoneNumber, outputFileFormat, c_options)
+    fileUtil.save_text_file(o_rdd, outputPath + "/" + Offer, outputFileFormat, c_options)
+    fileUtil.save_text_file(po_rdd, outputPath + "/" + PersonOrOrganization, outputFileFormat, c_options)
+    fileUtil.save_text_file(wp_rdd, outputPath + "/" + WebPage, outputFileFormat, c_options)
+    fileUtil.save_text_file(ea_rdd, outputPath + "/" + EmailAddress, outputFileFormat, c_options)
 
