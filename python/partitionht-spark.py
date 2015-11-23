2a142d893dca4b9ea291fdbddcc335d13f2e404f
Amandeep Singh <amandeep.s.saggu@gmail.com>
2015-11-23 14:55:35 -0800

    updating partition python file to read from karmaconfig

--- python/partitionht-spark.py
+++ python/partitionht-spark.py
@@ -1,25 +1,33 @@
-#!/usr/bin/env python
-
 from pyspark import SparkContext
-
 from optparse import OptionParser
 from fileUtil import FileUtil
-#from partitionht import PartitionHt
+import codecs
 import json
 
-if __name__ == "__main__":
+def processkarmaconfig(jsonkc):
+    classes = []
 
-    AdultService = "AdultService"
-    PhoneNumber = "PhoneNumber"
-    Offer = "Offer"
-    PersonOrOrganization = "PersonOrOrganization"
-    WebPage = "WebPage"
-    EmailAddress = "EmailAddress"
+    for config in jsonkc:
+        if "roots" in config:
+            for root in config["roots"]:
+                if "root" in root:
+                    classes.append(getclassnamefromroot(root["root"]))
+
+    print "Returning classes:", classes
+    return classes
+
+def getclassnamefromroot(root):
+    pieces = root.split("/")
+    classinstance = pieces[len(pieces)-1]
+    classname = classinstance[0:len(classinstance)-1]
+    return classname
+
+if __name__ == "__main__":
 
     sc = SparkContext(appName="DIG-FILTER-HT")
 
     usage = "usage: %prog [options] inputDataset inputDatasetFormat inputPath " \
-            "outputFilename outoutFileFormat"
+            "outputFilename outoutFileFormat karmaconfigpath"
     parser = OptionParser()
     parser.add_option("-r", "--separator", dest="separator", type="string",
                       help="field separator", default="\t")
@@ -30,28 +38,21 @@ if __name__ == "__main__":
     inputFileFormat = args[1]
     outputPath = args[2]
     outputFileFormat = args[3]
+    karmaconfigpath = args[4]
 
     fileUtil = FileUtil(sc)
+    kc = sc.textFile(karmaconfigpath).first()
+    
+
+    jsonkc = json.loads(kc)
+
+    classes = processkarmaconfig(jsonkc)
+
     input_rdd = fileUtil.load_json_file(inputFilename, inputFileFormat, c_options)
-    #partition = PartitionHt()
 
-    #as_rdd = input_rdd.mapValues(lambda x: partition.filter_docs(AdultService, x)).filter(lambda x: x[1] is not None).coalesce(42)
-    as_rdd = input_rdd.filter(lambda x: x[1]["a"] == AdultService).coalesce(21)
-    #pn_rdd = input_rdd.mapValues(lambda x: partition.filter_docs(PhoneNumber, x)).filter(lambda x: x[1] is not None).coalesce(42)
-    pn_rdd = input_rdd.filter(lambda x: x[1]["a"] == PhoneNumber).coalesce(21)
-    #o_rdd =  input_rdd.mapValues(lambda x: partition.filter_docs(Offer, x)).filter(lambda x: x[1] is not None).coalesce(42)
-    o_rdd =  input_rdd.filter(lambda x: x[1]["a"] == Offer).coalesce(21)
-    #po_rdd = input_rdd.mapValues(lambda x: partition.filter_docs(PersonOrOrganization, x)).filter(lambda x: x[1] is not None).coalesce(42)
-    po_rdd = input_rdd.filter(lambda x: x[1]["a"] == PersonOrOrganization).coalesce(21)
-    #wp_rdd = input_rdd.mapValues(lambda x: partition.filter_docs(WebPage, x)).filter(lambda x: x[1] is not None).coalesce(42)
-    wp_rdd = input_rdd.filter(lambda x: x[1]["a"] == WebPage).coalesce(21)
-    #ea_rdd = input_rdd.mapValues(lambda x: partition.filter_docs(EmailAddress, x)).filter(lambda x: x[1] is not None).coalesce(42)
-    ea_rdd = input_rdd.filter(lambda x: x[1]["a"] == EmailAddress).coalesce(21)
-
-    fileUtil.save_json_file(as_rdd, outputPath + "/"+ AdultService, outputFileFormat, c_options)
-    fileUtil.save_json_file(pn_rdd, outputPath + "/" + PhoneNumber, outputFileFormat, c_options)
-    fileUtil.save_json_file(o_rdd, outputPath + "/" + Offer, outputFileFormat, c_options)
-    fileUtil.save_json_file(po_rdd, outputPath + "/" + PersonOrOrganization, outputFileFormat, c_options)
-    fileUtil.save_json_file(wp_rdd, outputPath + "/" + WebPage, outputFileFormat, c_options)
-    fileUtil.save_json_file(ea_rdd, outputPath + "/" + EmailAddress, outputFileFormat, c_options)
-
+    
+    for classe in classes:
+        print "Save class:", classe
+        rdd_out = input_rdd.filter(lambda x: x[1]["a"] == classe).coalesce(21)
+        if not rdd_out.isEmpty():
+            fileUtil.save_json_file(rdd_out, outputPath + "/"+ classe, outputFileFormat, c_options)
